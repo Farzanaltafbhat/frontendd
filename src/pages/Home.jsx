558cd112325097fadf8aa86dc14e370472525146
Farzanaltafbhat <97350316+Farzanaltafbhat@users.noreply.github.com>
2024-01-30 21:33:17 -0800

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Home.css'
+import '../assets/images/location.png';
+import location from '../assets/images/location.png'
 const Home = () => {
   return (
-    <div className='banner'>
-
-     <h1>Home</h1>
+    <div className='search'>
+       <img src= {location} alt="Location Icon"  />
+      <input
+          type="text"
+          id="searchInput"
+          placeholder="Type your location"
+          className='location'
+        />
+        <input
+          type="text"
+          id="searchInput1"
+          placeholder="  Search for doctors"
+          className='doctor'
+        />
+        <button className='book'>
+            Book Consulation
+        </button>
     </div>
   )
 }
